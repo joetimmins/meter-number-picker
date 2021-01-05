279f79be29c69b4283d56c8c6615e54395a8c993
Joe Timmins <joe@tickr.co.uk>
2021-01-05 21:55:59 +0000

    Enable view binding, add button

---
+++ sample/src/main/java/com/alexzaitsev/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.alexzaitsev.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alexzaitsev.sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.setTopGuy.setOnClickListener {
+            binding.meterView1.value = 5678
+        }
+    }
+}
